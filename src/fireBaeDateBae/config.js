43637dfd2aec7d57cf48f6f81f1e2419500f4f2e
Vishwak-Kiran <vishwakkiran2002@gmail.com>
2022-05-10 21:43:25 +0530

    final

---
+++ src/fireBaeDateBae/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTe0ImyJUEyuyloLwOaapTge1n06b8Uhk",
+  authDomain: "loanlender-48895.firebaseapp.com",
+  projectId: "loanlender-48895",
+  storageBucket: "loanlender-48895.appspot.com",
+  messagingSenderId: "1032474568407",
+  appId: "1:1032474568407:web:3adf755bb66f34134c3292",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const fireStore = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const timestamp = firebase.firestore.Timestamp;
+
+
+export { fireStore, auth, timestamp, storage };
